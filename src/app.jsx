dd72af7af4bbed007cd7e1eb84a118923d0e9bc3
Jonathan Schoonhoven <jtschoonhoven@gmail.com>
2018-11-21 18:34:37 -0800

    content to jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,8 @@
-import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles/main.scss';
-import exampleAction from './actions';
-import exampleComponent from './components';
-import exampleContainer from './containers';
-import exampleReducer from './reducers';
-
-
-// testing
-exampleAction();
-exampleComponent();
-exampleContainer();
-exampleReducer();
+import ArchiveLogin from './components/login/index.jsx';
 
 
 const element = document.createElement('div');
@@ -22,6 +11,6 @@ document.body.appendChild(element);
 
 
 ReactDOM.render(
-    <p>&copy; Jonathan Schoonhoven 2018</p>,
-    document.getElementById('root'),
+    <ArchiveLogin />,
+    document.getElementById('archive-content'),
 );
---
+++ src/components/login/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+class ArchiveLogin extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1 className="display-4">Welcome!</h1>
+                <p className="lead">
+                    This is a simple hero unit, a simple jumbotron-style component for
+                    calling extra attention to featured content or information.
+                </p>
+                <hr className="my-4" />
+                <p>
+                    It uses utility classes for typography and spacing to space content
+                    out within the larger container.
+                </p>
+                <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </div>
+        );
+    }
+}
+
+
+export default ArchiveLogin;
