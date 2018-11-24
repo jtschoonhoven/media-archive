f5a97d5fd81d5f009071ed653c80858cfd41bade
Jonathan Schoonhoven <jtschoonhoven@gmail.com>
2018-11-24 11:06:17 -0800

    cache control

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 
-
+// constants
 const PORT = 8081; // nginx default
 const STATIC_PATH = path.resolve(__dirname, '../dist');
+const STATIC_OPTS = { maxAge: '5s' };
 const HTML_PATH = path.resolve(__dirname, '../dist/index.html');
-const HTML_OPTS = {};
+const HTML_OPTS = { maxAge: '5s' };
 
 // init app
 const app = express();
@@ -16,7 +17,7 @@ app.get('/healthcheck', (req, res) => {
 });
 
 // first attempt to resolve URL to file in static dir
-app.use(express.static(STATIC_PATH));
+app.use(express.static(STATIC_PATH, STATIC_OPTS));
 
 // if no file matches URL in static dir, resolve to app
 app.get('*', (req, res) => {
