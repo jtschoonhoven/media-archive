aea4c6c2d2090c9e1480d369e7de188b6ea9357f
jtschoonhoven <jtschoonhoven@gmail.com>
2019-07-07 23:16:35 -0700

    refactor uploads service

--- server/services/uploads.js
+++ server/services/uploads.js
@@ -11,116 +11,6 @@ const filesService = require('./files');
 const UPLOAD_STATUSES = config.get('CONSTANTS.UPLOAD_STATUSES');
 
 
-function getInsertSql(
-    uploadBatchId,
-    mediaFileUnsafe,
-    mediaName,
-    mediaType,
-    mediaFile,
-    mediaPath,
-    mediaExtn,
-    mediaSize,
-    userEmail,
-) {
-    const uuid = uuidv4(); // generate secure, random uuid
-    const mediaUrl = s3Service.getS3Url(uuid, mediaExtn);
-    return sql`
-        INSERT INTO media (
-            uuid,
-            media_name,
-            media_url,
-            media_type,
-            media_file_name,
-            media_file_name_unsafe,
-            media_file_path,
-            media_file_extension,
-            media_file_size_bytes,
-            upload_email,
-            upload_status,
-            upload_batch_id,
-            upload_started_at
-        )
-        VALUES (
-            ${uuid}, -- uuid
-            ${mediaName}, -- media_name
-            ${mediaUrl}, -- media_url
-            ${mediaType}, -- media_type
-            ${mediaFile}, -- media_file_name
-            ${mediaFileUnsafe}, -- media_file_name_unsafe
-            ${mediaPath}, -- media_file_path
-            ${mediaExtn}, -- media_file_extension
-            ${mediaSize}, -- media_file_size_bytes
-            ${userEmail}, -- upload_email
-            ${UPLOAD_STATUSES.PENDING}, -- upload_status
-            ${uploadBatchId}, -- upload_batch_id
-            NOW() -- upload_started_at
-        );
-    `;
-}
-
-/*
- * Throw an error if the DB already contains a file at the same path as an upload in fileList.
- */
-async function checkDuplicates(dirPath, fileList) {
-    const query = sql`
-        SELECT *
-        FROM media
-        WHERE deleted_at IS NULL
-        AND upload_status = ${UPLOAD_STATUSES.SUCCESS}
-        AND deleted_at IS NULL
-        AND media_file_path LIKE ${dirPath} || '/%'
-        AND (FALSE
-    `;
-    fileList.forEach((fileInfo) => {
-        query.append(sql`OR media_file_name_unsafe = ${fileInfo.name}\n`);
-    });
-    query.append(sql`)`);
-    const result = await db.all(query);
-    if (result.length) {
-        const dupeName = result[0].media_name;
-        const dupePath = result[0].media_file_path;
-        throw new Error(`Cannot upload batch with duplicate file "${dupeName}" at "${dupePath}"`);
-    }
-}
-
-async function addFilesToDb(dirPath, fileList, userEmail) {
-    const uploadBatchId = uuidv4();
-    const transaction = new db.Transaction();
-
-    try {
-        fileList.forEach((fileInfo) => {
-            // validate and sanitize uploaded file info
-            const mediaName = filesService.getFileTitle(fileInfo.name);
-            const mediaType = filesService.getFileType(fileInfo.name);
-            const mediaFile = filesService.getSanitizedFileName(fileInfo.name);
-            const mediaExtn = filesService.getFileExtension(fileInfo.name);
-            const mediaPath = filesService.getSanitizedFilePath(path.join(dirPath, mediaFile));
-            const mediaSize = parseInt(fileInfo.sizeInBytes, 10);
-
-            // exectute query within a transaction
-            const query = getInsertSql(
-                uploadBatchId,
-                fileInfo.name,
-                mediaName,
-                mediaType,
-                mediaFile,
-                mediaPath,
-                mediaExtn,
-                mediaSize,
-                userEmail,
-            );
-            transaction.add(query);
-        });
-    }
-    catch (err) {
-        logger.error(`rolling back files upload: "${err.message}"`);
-        await transaction.rollback();
-        throw err;
-    }
-    await transaction.commit();
-    return uploadBatchId;
-}
-
 /*
  * Add pending upload to media_uploads_pending table and return direct upload auth for S3.
  */
@@ -202,3 +92,113 @@ module.exports.cancel = async (fileId, userEmail) => {
     // FIXME: this shouldn't return as successful if nothing was deleted
     return {};
 };
+
+async function addFilesToDb(dirPath, fileList, userEmail) {
+    const uploadBatchId = uuidv4();
+    const transaction = new db.Transaction();
+
+    try {
+        fileList.forEach((fileInfo) => {
+            // validate and sanitize uploaded file info
+            const mediaName = filesService.getFileTitle(fileInfo.name);
+            const mediaType = filesService.getFileType(fileInfo.name);
+            const mediaFile = filesService.getSanitizedFileName(fileInfo.name);
+            const mediaExtn = filesService.getFileExtension(fileInfo.name);
+            const mediaPath = filesService.getSanitizedFilePath(path.join(dirPath, mediaFile));
+            const mediaSize = parseInt(fileInfo.sizeInBytes, 10);
+
+            // exectute query within a transaction
+            const query = _getInsertSql(
+                uploadBatchId,
+                fileInfo.name,
+                mediaName,
+                mediaType,
+                mediaFile,
+                mediaPath,
+                mediaExtn,
+                mediaSize,
+                userEmail,
+            );
+            transaction.add(query);
+        });
+    }
+    catch (err) {
+        logger.error(`rolling back files upload: "${err.message}"`);
+        await transaction.rollback();
+        throw err;
+    }
+    await transaction.commit();
+    return uploadBatchId;
+}
+
+/*
+ * Throw an error if the DB already contains a file at the same path as an upload in fileList.
+ */
+async function checkDuplicates(dirPath, fileList) {
+    const query = sql`
+        SELECT *
+        FROM media
+        WHERE deleted_at IS NULL
+        AND upload_status = ${UPLOAD_STATUSES.SUCCESS}
+        AND deleted_at IS NULL
+        AND media_file_path LIKE ${dirPath} || '/%'
+        AND (FALSE
+    `;
+    fileList.forEach((fileInfo) => {
+        query.append(sql`OR media_file_name_unsafe = ${fileInfo.name}\n`);
+    });
+    query.append(sql`)`);
+    const result = await db.all(query);
+    if (result.length) {
+        const dupeName = result[0].media_name;
+        const dupePath = result[0].media_file_path;
+        throw new Error(`Cannot upload batch with duplicate file "${dupeName}" at "${dupePath}"`);
+    }
+}
+
+function _getInsertSql(
+    uploadBatchId,
+    mediaFileUnsafe,
+    mediaName,
+    mediaType,
+    mediaFile,
+    mediaPath,
+    mediaExtn,
+    mediaSize,
+    userEmail,
+) {
+    const uuid = uuidv4(); // generate secure, random uuid
+    const mediaUrl = s3Service.getS3Url(uuid, mediaExtn);
+    return sql`
+        INSERT INTO media (
+            uuid,
+            media_name,
+            media_url,
+            media_type,
+            media_file_name,
+            media_file_name_unsafe,
+            media_file_path,
+            media_file_extension,
+            media_file_size_bytes,
+            upload_email,
+            upload_status,
+            upload_batch_id,
+            upload_started_at
+        )
+        VALUES (
+            ${uuid}, -- uuid
+            ${mediaName}, -- media_name
+            ${mediaUrl}, -- media_url
+            ${mediaType}, -- media_type
+            ${mediaFile}, -- media_file_name
+            ${mediaFileUnsafe}, -- media_file_name_unsafe
+            ${mediaPath}, -- media_file_path
+            ${mediaExtn}, -- media_file_extension
+            ${mediaSize}, -- media_file_size_bytes
+            ${userEmail}, -- upload_email
+            ${UPLOAD_STATUSES.PENDING}, -- upload_status
+            ${uploadBatchId}, -- upload_batch_id
+            NOW() -- upload_started_at
+        );
+    `;
+}
